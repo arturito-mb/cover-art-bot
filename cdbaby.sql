da028618470e4eecb94ef748676bbd6f7db410a7
arturito <arturito@arturito>
2012-06-17 06:41:35 +0100

    add sql files and script for running arturito

---
+++ cdbaby.sql
@@ -0,0 +1,34 @@
+SET search_path TO musicbrainz;
+SELECT r.gid, regexp_replace(url, 'http://cdbaby.com/cd/(.)(.)(.*)', E'http://images.cdbaby.name/\\1/\\2/\\1\\2\\3.jpg'), NULL, NULL, lru.id
+FROM url
+JOIN l_release_url lru ON lru.entity1 = url.id
+JOIN release r ON r.id = lru.entity0
+JOIN link l ON l.id = lru.link
+JOIN link_type lt ON lt.id = l.link_type
+WHERE url ~ E'^http://cdbaby.com/'
+AND lt.name = 'cover art link'
+AND lru.edits_pending = 0
+
+-- exclude releases with more than one cover art URL
+AND lru.entity0 NOT IN (
+	SELECT entity0
+	FROM l_release_url lru
+	JOIN url ON url.id = entity1
+	JOIN link l ON l.id = lru.link
+	JOIN link_type lt ON lt.id = l.link_type
+	WHERE lt.name = 'cover art link'
+	GROUP BY entity0 HAVING COUNT(*) > 1
+)
+
+-- exclude URLs linked to more than one release
+AND lru.entity1 NOT IN (
+	SELECT entity1
+	FROM l_release_url lru
+	JOIN url ON url.id = entity1
+	JOIN link l ON l.id = lru.link
+	JOIN link_type lt ON lt.id = l.link_type
+	WHERE lt.name = 'cover art link'
+	GROUP BY entity1 HAVING COUNT(*) > 1
+)
+
+ORDER BY url;
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MAX=${2:-100}
+
+case $1 in
+	cdbaby )
+		psql -qtA -F $'\t' musicbrainz < cdbaby.sql > /tmp/data-cdbaby && perl bot.pl --max=$MAX --note="Added from existing cdbaby.com cover art relationship. The release has only one cover art relationship and the URL is only linked to one release." --remove-note="Added to CAA from existing cdbaby.com cover art relationship. The release has only one cover art relationship and the URL is only linked to one release. The release is no longer available on cdbaby.com so this relationship cannot be converted into another relationship." /tmp/data-cdbaby arturito
+	;;
+
+	liveweb )
+		echo $MAX
+		psql -qtA -F $'\t' musicbrainz < liveweb.sql > /tmp/data-liveweb && perl bot.pl --max=$MAX --note="Added from existing liveweb.archive.org cover art relationship. The release has only one cover art relationship and the URL is only linked to one release." --remove-note="Added to CAA from existing liveweb.archive.org cover art relationship. The release has only one cover art relationship and the URL is only linked to one release." /tmp/data-liveweb arturito
+	;;
+
+	* )
+		echo "Nothing to do"
+	;;
+esac
+
