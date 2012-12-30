d61c6b7d1865dd0c995276d5ae13c3793d5a6ece
arturito <arturito@arturito>
2012-12-30 16:41:10 +0100

    add web.archive.org queries

--- run.sh
+++ run.sh
@@ -28,6 +28,9 @@ case $1 in
 		psql -qtA -F $'\t' musicbrainz < $1.sql > /tmp/data-$1 && perl bot.pl --max=$MAX --note="Added from existing www.archive.org cover art relationship (URL: {$url}). The release has only one cover art relationship and the URL is only linked to one release. The archive.org directory contains an image (image dimensions: {$x_dim}x{$y_dim}) but no music files." --remove-note="Added from existing www.archive.org cover art relationship (URL: {$url}). The release has only one cover art relationship and the URL is only linked to one release. The archive.org directory contains an image (image dimensions: {$x_dim}x{$y_dim}) but no music files." --archive-no-music --use-front /tmp/data-$1 arturito
 	;;
 
+	webarchiveorg )
+		psql -qtA -F $'\t' musicbrainz < $1.sql > /tmp/data-$1 && perl bot.pl --max=$MAX --note="Added from existing web.archive.org cover art relationship (URL: {$url}). The release has only one cover art relationship and the URL is only linked to one release. Image size is at least 250x250 (actual dimensions: {$x_dim}x{$y_dim})." --remove-note="Added to CAA from existing web.archive.org cover art relationship (URL: {$url}). The release has only one cover art relationship and the URL is only linked to one release. Image size is at least 250x250 (actual dimensions: {$x_dim}x{$y_dim})." --image-size=250 /tmp/data-$1 arturito
+	;;
 
 	* )
 		echo "Nothing to do"
